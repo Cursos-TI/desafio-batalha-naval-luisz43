8e291ceada745989dc35edbaa8c7216dd48ac31d
luiz eduardo damasceno <145232349+luisz43@users.noreply.github.com>
2025-05-22 15:24:52 -0400

    Create batalha naval.c

---
+++ batalha naval.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define TAMANHO_TABULEIRO 10
+#define TAMANHO_NAVIO 3
+
+void inicializarTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            tabuleiro[i][j] = 0;  // 0 representa água
+        }
+    }
+}
+
+void posicionarNavioHorizontal(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna) {
+    if (coluna + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            tabuleiro[linha][coluna + i] = 3;  // 3 representa navio
+        }
+    }
+}
+
+void posicionarNavioVertical(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna) {
+    if (linha + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            tabuleiro[linha + i][coluna] = 3;  // 3 representa navio
+        }
+    }
+}
+
+void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
+    
+    inicializarTabuleiro(tabuleiro);
+
+    // Coordenadas iniciais dos navios (definidas diretamente)
+    int linhaHorizontal = 2, colunaHorizontal = 3;
+    int linhaVertical = 5, colunaVertical = 7;
+
+    posicionarNavioHorizontal(tabuleiro, linhaHorizontal, colunaHorizontal);
+    posicionarNavioVertical(tabuleiro, linhaVertical, colunaVertical);
+
+    exibirTabuleiro(tabuleiro);
+
+    return 0;
+}
